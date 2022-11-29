555cf8a593ff1a2effbae8f77f754d5ddcf78775
Youssef Jehbali <85554678+youssefjeh@users.noreply.github.com>
2022-11-29 10:09:40 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,16 +1,16 @@
 def buildJar() {
      echo 'building Jar file...'
-     sh 'mvn install:install-file -DgroupId=javax.jms -DartifactId=jms -Dversion=1.1 -Dpackaging=jar -Dfile=jms-1.1.jar'
-     sh 'mvn install:install-file -DgroupId="javax.jms" -DartifactId="jms" -Dversion="1.1" -Dpackaging="jar" -Dfile="jms-1.1.jar"'
-     sh 'mvn package'
+     //sh 'mvn install:install-file -DgroupId=javax.jms -DartifactId=jms -Dversion=1.1 -Dpackaging=jar -Dfile=jms-1.1.jar'
+     //sh 'mvn install:install-file -DgroupId="javax.jms" -DartifactId="jms" -Dversion="1.1" -Dpackaging="jar" -Dfile="jms-1.1.jar"'
+    // sh 'mvn package'
 }
 
-def buildImg() {
-    echo 'building docker image ...'
-    withCredentials([usernamePassword(credentialsID: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t dockerysf/my-app:3.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push dockerysf/my-app:3.0'
+// def buildImg() {
+   // echo 'building docker image ...'
+    //withCredentials([usernamePassword(credentialsID: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+      //  sh 'docker build -t dockerysf/my-app:3.0 .'
+       // sh "echo $PASS | docker login -u $USER --password-stdin"
+        //sh 'docker push dockerysf/my-app:3.0'
     }
 }
 
