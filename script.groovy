4399d7cee8c372612ff26ae12cce6d3088ab87d7
Youssef Jehbali <85554678+youssefjeh@users.noreply.github.com>
2022-11-29 10:21:25 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,17 +1,5 @@
-def buildJar() {
-     echo 'building Jar file...'
-     //sh 'mvn install:install-file -DgroupId=javax.jms -DartifactId=jms -Dversion=1.1 -Dpackaging=jar -Dfile=jms-1.1.jar'
-     //sh 'mvn install:install-file -DgroupId="javax.jms" -DartifactId="jms" -Dversion="1.1" -Dpackaging="jar" -Dfile="jms-1.1.jar"'
-    // sh 'mvn package'
-}
-
-// def buildImg() {
-   // echo 'building docker image ...'
-    //withCredentials([usernamePassword(credentialsID: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-      //  sh 'docker build -t dockerysf/my-app:3.0 .'
-       // sh "echo $PASS | docker login -u $USER --password-stdin"
-        //sh 'docker push dockerysf/my-app:3.0'
-    }
+def buildApp() {
+     echo 'building app...'
 }
 
 def testApp() {
